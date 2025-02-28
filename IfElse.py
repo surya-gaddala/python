7fa52154a526a980e5757e10231ae4c740248ea0
surya <gaddala.surya@dragonflytest.com>
2025-02-28 18:42:34 +0530

    modified:   IfElse.py

--- IfElse.py
+++ IfElse.py
@@ -1,15 +1,20 @@
-a = int(input("Enter age: "))
-print(a)
+age = input("Enter age: ")
 
-if a <= 0:
-    print("Enter a valid age")
-elif 0 < a <= 10:
-    print("You are a kid")
-elif 11 <= a <= 19:
-    print("You are a teenager")
-elif 20 <= a <= 35:
-    print("You are a young adult")
-elif 36 <= a <= 60:
-    print("You are a middle aged person")
+if age.isdigit():  # Check if input contains only digits
+    a = int(age)
+    print(a)
+
+    if a <= 0:
+        print("Enter a valid age")
+    elif 0 < a <= 10:
+        print("You are a kid")
+    elif 11 <= a <= 19:
+        print("You are a teenager")
+    elif 20 <= a <= 35:
+        print("You are a young adult")
+    elif 36 <= a <= 60:
+        print("You are a middle-aged person")
+    else:
+        print("You are a senior citizen")
 else:
-    print("You are a senior citizen")
+    print("Invalid input! Please enter a valid numeric age.")
