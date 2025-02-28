7742a690d928e15415adce9338378fb946f80903
surya <gaddala.surya@dragonflytest.com>
2025-02-28 14:57:39 +0530

    new file:   StringMethods.py

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+a =" Surya "
+print(len(a)) #length of the string
+print(a.upper()) #converts to upper case
+print(a.lower()) #converts to lower case
+print(a[3]) #prints the first character of the string
+print(a.rstrip(" ")) #removes the trailing white spaces
+print(a.lstrip()) #removes the leading white spaces
+print(a.strip()) #removes the leading and trailing white spaces
+print(a.isalnum()) #checks if the string is alphanumeric
+Space=" " 
+print(Space.isspace()) #checks if the string is space
+print(a.replace("Surya","Raj")) #replaces the string with another string
